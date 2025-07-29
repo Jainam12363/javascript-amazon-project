582e3b132a5e035ea23ca2308ad0ad970ea91f03
Jainam12363 <jainamk62@gmail.com>
2025-07-29 12:21:18 +0530

    Made delivery options interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from "../data/cart.js";
+import { cart, removeFromCart, updateDeliveryOption } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
@@ -90,18 +90,20 @@ function deliveryOptionsHTML(matchingProduct, cartItem){
         const isChecked= deliveryOption.id===cartItem.deliveryOptionId;
 
         html+=`
-        <div class="delivery-option">
+        <div class="delivery-option js-delivery-option" 
+            data-product-id="${matchingProduct.id}" 
+            data-delivery-option-id="${deliveryOption.id}">
             <input type="radio"
-            ${isChecked?'checked':''}
-            class="delivery-option-input"
-            name="delivery-option-${matchingProduct.id}">
+                ${isChecked?'checked':''}
+                class="delivery-option-input"
+                name="delivery-option-${matchingProduct.id}">
             <div>
-            <div class="delivery-option-date">
-                ${dateString}
-            </div>
-            <div class="delivery-option-price">
-                ${priceString} Shipping
-            </div>
+                <div class="delivery-option-date">
+                    ${dateString}
+                </div>
+                <div class="delivery-option-price">
+                    ${priceString} Shipping
+                </div>
             </div>
         </div>
         `
@@ -123,4 +125,11 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
         
     });
 
+});
+
+document.querySelectorAll('.js-delivery-option').forEach((element)=>{
+    element.addEventListener('click',()=>{
+        const {productId,deliveryOptionId}=element.dataset;
+        updateDeliveryOption(productId,deliveryOptionId);
+    });
 });
