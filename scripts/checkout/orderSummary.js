683ac47db2f210a58740814066565a9e4217c864
Jainam12363 <jainamk62@gmail.com>
2025-07-29 21:37:36 +0530

    Made payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 
 const today= dayjs();// this is import of external library which brings current date
@@ -110,6 +111,7 @@ export function renderOrderSummary(){
                 `.js-cart-item-container-${productId}`
             );
             container.remove();
+            renderPaymentSummary();
             
         });
 
@@ -120,6 +122,7 @@ export function renderOrderSummary(){
             const {productId,deliveryOptionId}=element.dataset;
             updateDeliveryOption(productId,deliveryOptionId);
             renderOrderSummary();  //this runs entire code again so that all elements are refreshed
+            renderPaymentSummary();
         });
     });
 }
